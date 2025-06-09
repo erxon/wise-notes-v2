793bd4ef5e6a3cd33b0172e6a5025445a5686696
Ericson Castasus <castasuse@gmail.com>
2025-06-09 13:55:48 +0800

    readd protected route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -2,7 +2,13 @@ const { Router } = require("express");
 const router = Router();
 const passport = require("passport");
 
-router.post("/signin", passport.authenticate("local"));
+router.post(
+  "/signin",
+  passport.authenticate("local", {
+    successRedirect: "/api/v1/protected",
+    failureRedirect: "/",
+  })
+);
 
 router.get("/signout", (req, res) => {
   req.logout(() => res.send("Logged out"));
