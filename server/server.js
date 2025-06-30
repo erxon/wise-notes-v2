b2724744c5213a357528dbe419d7f4ddf7508908
Ericson Castasus <castasuse@gmail.com>
2025-06-30 14:08:33 +0800

    cors origin

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: `http://localhost:5173`,
+    origin: process.env.ORIGIN,
     credentials: true,
   })
 );
