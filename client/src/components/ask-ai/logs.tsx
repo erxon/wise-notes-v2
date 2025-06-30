a35cbe0113eb02018c88c1a1a5ba94a3a23d66db
Ericson Castasus <castasuse@gmail.com>
2025-06-30 17:33:05 +0800

    create component for answer

--- client/src/components/ask-ai/logs.tsx
+++ client/src/components/ask-ai/logs.tsx
@@ -3,7 +3,6 @@ import { markdownLookBack } from "@llm-ui/markdown";
 import { useStreamExample, throttleBasic } from "@llm-ui/react";
 import Markdown from "./markdown";
 import type { Chat } from "@/lib/types";
-import { useEffect, useState } from "react";
 
 const throttle = throttleBasic({
   readAheadChars: 5,
@@ -14,16 +13,17 @@ const throttle = throttleBasic({
 });
 
 export default function Logs({ chat }: { chat: Chat }) {
-  const [chatState, setChatState] = useState<Chat>(chat);
-  const [answer, setAnswer] = useState<string>("");
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="bg-gray-100 dark:bg-black dark:outline rounded-full p-3 w-fit self-end">
+        {chat.query}
+      </div>
+      <Answer answer={chat.answer} />
+    </div>
+  );
+}
 
-  useEffect(() => {
-    const [thinking, actualAnswer] = chat.answer.split("</think>");
-
-    setChatState(chat);
-    setAnswer(actualAnswer);
-  }, [chat]);
-
+function Answer({ answer }: { answer: string }) {
   const { isStreamFinished, output } = useStreamExample(answer);
   const { blockMatches } = useLLMOutput({
     llmOutput: output,
@@ -37,16 +37,11 @@ export default function Logs({ chat }: { chat: Chat }) {
   });
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="bg-gray-100 dark:bg-black dark:outline rounded-full p-3 w-fit self-end">
-        {chatState.query}
-      </div>
-      <div>
-        {blockMatches.map((blockMatch, index) => {
-          const Component = blockMatch.block.component;
-          return <Component key={index} blockMatch={blockMatch} />;
-        })}
-      </div>
+    <div>
+      {blockMatches.map((blockMatch, index) => {
+        const Component = blockMatch.block.component;
+        return <Component key={index} blockMatch={blockMatch} />;
+      })}
     </div>
   );
 }
