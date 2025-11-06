b717cdbd2dbc3a3a483d54b3b5cabfd4515ea33d
Ericson Castasus <castasuse@gmail.com>
2025-11-06 19:10:23 +0800

    fix error in HuggingFaceInferenceEmbeddings

--- server/utilities/embeddings.util.js
+++ server/utilities/embeddings.util.js
@@ -1,11 +1,12 @@
 const {
   HuggingFaceInferenceEmbeddings,
 } = require("@langchain/community/embeddings/hf");
+
 require("dotenv").config();
 
 const embeddings = new HuggingFaceInferenceEmbeddings({
   apiKey: process.env.HUGGINGFACE_API_KEY,
   model: "BAAI/bge-small-en-v1.5",
+  endpointUrl: "https://router.huggingface.co/hf-inference/models/BAAI/bge-small-en-v1.5/pipeline/feature-extraction",
 });
-
 module.exports = embeddings;
